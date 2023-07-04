6a42a34f3f1d67b2814cf0d6055d17b93b08a56a
Alper YALÇIN <faewox@Alper-MacBook-Air.local>
2023-07-04 15:40:38 +0300

    google tag

--- src/app/(home)/layout.jsx
+++ src/app/(home)/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 import Header from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
 import { fetchUser } from '@/services/users'
@@ -13,6 +14,15 @@ export default async function RootLayout({ children }) {
   const { user } = await fetchUser(process.env.OWNER_ID)
   return (
     <html lang="tr">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BW624J0MB1" />
+      <Script>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BW624J0MB1');
+          `}
+      </Script>
       <body className={`flex flex-col`}>
         <Header user={user} />
         <main>
